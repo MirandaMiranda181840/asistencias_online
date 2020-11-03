d256397331f5c8a611f360d8d2df73a2459674dc
Miranda Miranda <miranda.miranda@potros.itson.edu.mx>
2020-11-02 23:21:54 -0700

    Version 1 - Agregar curso

---
+++ asistencias_cursos/src/entidades/Curso.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author miran
+ */
+public class Curso {
+    
+    private String nombre;
+    private String periodo;
+    private String dias;
+    private String hora;
+
+    public Curso(String nombre, String periodo, String dias, String hora) {
+        this.nombre = nombre;
+        this.periodo = periodo;
+        this.dias = dias;
+        this.hora = hora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getDias() {
+        return dias;
+    }
+
+    public void setDias(String dias) {
+        this.dias = dias;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    
+    
+    
+}
---
+++ asistencias_cursos/src/servicios/Conexion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+
+/**
+ *
+ * @author miran
+ */
+
+import java.sql.*;
+
+public class Conexion {
+   private static Connection cnx = null;
+   
+   public static Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost/asistencias", "root", "1234");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
+      
+      
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+   
+   public boolean ifExists(String sSQL, String nombre, String periodo, String dias, String hora) throws SQLException {
+    PreparedStatement ps = cnx.prepareStatement(sSQL);
+    ps.setString(1, nombre);
+    ps.setString(2, periodo);
+    ps.setString(3, dias);
+    ps.setString(4, hora);
+    ResultSet rs = ps.executeQuery();
+    return rs.next();
+}
+}
